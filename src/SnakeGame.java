53ab7fecba3e8318f5a38040206954190d802ea4
Yashasvi Choudhary <choudharyyashasvi1009@gmail.com>
2025-12-15 11:40:25 +0530

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class SnakeGame extends JFrame { // inherit JFrame
+    SnakeGame() { // constructor class
+        // use jframe from swing package to make the frame of the game.
+
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame(); // anonymous class -> calls constructor class
+    }
+}
